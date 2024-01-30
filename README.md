8693aae6d89215310c54f4070feb35795ce180c8
sohailahmedjokhio <139850469+sohailahmedjokhio@users.noreply.github.com>
2024-01-30 06:12:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Treasure Island Game
+
+Welcome to the Treasure Island game! Your mission, should you choose to accept it, is to find the treasure hidden on the island. Be careful, as your choices will determine your fate. Good luck!
+
+# How to Play
+
+## Step 1: Download the Game
+1. Make sure you have Python installed on your computer. If not, you an download it from [python.org](https://www.python.org/).
+
+2. Clone this repository to your local machine using Git:
+  <pre>git clone https://github.com/yourusername/treasure-island-game.git</pre>
+Alternatively, you can download the ZIP file of the repository and extract it to your preferred location.
+
+## Step 2: Run the Game
+1. Open a terminal or command prompt.
+2. Navigate to the directory where you downloaded or cloned the game.
+   <pre>cd path/to/treasure-island-game</pre>
+3. Run the game script using Python:
+   <pre>python treasure_island.py</pre>
+If you're using Python 3, you might need to use 'python3' instead of 'python':
+  <pre>python3 treasure_island.py</pre>
+
+## Step 3: Play the Game
+
+1. Follow the on-screen instructions.
+2. Make choices by typing the specified keywords.
+3. Discover your fate in the Treasure Island!
+
+# Contributing
+If you'd like to contribute to the game, feel free to fork the repository and submit a pull request. Your contributions are highly appreciated!
+
+# License
+This game is released under the [MIT License](https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt). Feel free to modify and distribute it as you please, but please include the original license file.
---
+++ main.py
@@ -0,0 +1,58 @@
+print('''
+                  __..-----')
+        ,.--._ .-'_..--...-'
+       '-"'. _/_ /  ..--''""'-.
+       _.--""...:._:(_ ..:"::. \
+    .-' ..::--""_(##)#)"':. \ \)    \ _|_ /
+   /_:-:'/  :__(##)##)    ): )   '-./'   '\.-'
+   "  / |  :' :/""\///)  /:.'    --(       )--
+     / :( :( :(   (#//)  "       .-'\.___./'-.
+    / :/|\ :\_:\   \#//\            /  |  \
+    |:/ | ""--':\   (#//)              '
+    \/  \ :|  \ :\  (#//)
+         \:\   '.':. \#//\
+          ':|    "--'(#///)
+                     (#///)
+                     (#///)         ___/""\     
+                      \#///\           oo##
+                      (##///)         `-6 #
+                      (##///)          ,'`.
+                      (##///)         // `.\
+                      (##///)        ||o   \\
+                       \##///\        \-+--//
+                       (###///)       :_|_(/
+                       (sjw////)__...--:: :...__
+                       (#/::''        :: :     ""--.._
+                  '__..-''           __;: :            "-._
+          __..--""                  `---/ ;                '._
+ ___..--""                             `-'                    "-..___
+   (_ ""---....___                                     __...--"" _)
+     """--...  ___"""""-----......._______......----"""     --"""
+                   """"       ---.....   ___....----
+''')
+
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+print("You're at a crossroad. where do you want to go? Type 'left' or 'right'")
+action1 = input().lower()
+if action1 == "left":
+    print("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+    action2 = input().lower()
+    if action2 == "wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?")
+        action3 = input().lower()
+        if action3 == "yellow":
+            print("Congratulations, You Win.")
+        elif action3 == "red":
+            print("Burned by fire.")
+            print("Game Over.")
+        else:
+            print("Eaten by beasts.")
+            print("Game Over.")
+    else:
+        print("Attacked by trout.")
+        print("Game Over")
+else:
+    print("Fall into a Hole.")
+    print("Game Over")
