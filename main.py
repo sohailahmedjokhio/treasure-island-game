c128f67b1f59396caa970829ac596748da989863
sohailahmedjokhio <139850469+sohailahmedjokhio@users.noreply.github.com>
2024-01-30 06:22:26 +0000

    Updated main.py and README.md file

--- main.py
+++ main.py
@@ -37,10 +37,14 @@ print("Your mission is to find the treasure.")
 print("You're at a crossroad. where do you want to go? Type 'left' or 'right'")
 action1 = input().lower()
 if action1 == "left":
-    print("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.")
+    print(
+        "You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across."
+    )
     action2 = input().lower()
     if action2 == "wait":
-        print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?")
+        print(
+            "You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose?"
+        )
         action3 = input().lower()
         if action3 == "yellow":
             print("Congratulations, You Win.")
